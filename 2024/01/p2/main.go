2c86343d13541fb951d2b1e6c0dfd41b29566c47
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2024-12-01 18:55:09 +0100

    a dict is a better solution

--- 2024/01/p2/main.go
+++ 2024/01/p2/main.go
@@ -7,22 +7,16 @@ import (
 
 func main() {
 	arr0 := []int{}
-	arr1 := []int{}
+    dict1 := make(map[int]int)
 	util.StreamFile("input", func(line string) {
 		var val0, val1 int
 		fmt.Sscanf(line, "%d   %d", &val0, &val1)
 		arr0 = append(arr0, val0)
-		arr1 = append(arr1, val1)
+        dict1[val1] = dict1[val1] + 1
 	})
 	sumSimilarity := 0
 	for _, val0 := range arr0 {
-		valSimilarity := 0
-		for _, val1 := range arr1 {
-			if val0 == val1 {
-				valSimilarity += 1
-			}
-		}
-		sumSimilarity += valSimilarity * val0
+        sumSimilarity += dict1[val0] * val0
 	}
 	fmt.Println(sumSimilarity)
 }
