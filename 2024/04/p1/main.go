7e608b08db337062b9ee4866f019346d486e3976
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2024-12-04 20:00:47 +0100

    fmt

--- 2024/04/p1/main.go
+++ 2024/04/p1/main.go
@@ -10,38 +10,38 @@ func main() {
 	sum := 0
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[i]); j++ {
-            // east
-            if len(grid[i]) >= j+4 && "XMAS" == grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i][j+3] {
-                sum += 1
-            }
-            // southeast
-            if len(grid) >= i+4 && len(grid[i]) >= j+4 && "XMAS" == grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2]+grid[i+3][j+3] {
-                sum += 1
-            }
-            // south
-            if len(grid) >= i+4 && "XMAS" == grid[i][j]+grid[i+1][j]+grid[i+2][j]+grid[i+3][j] {
-                sum += 1
-            }
-            // southwest
-            if len(grid) >= i+4 && j-4 >= -1 && "XMAS" == grid[i][j]+grid[i+1][j-1]+grid[i+2][j-2]+grid[i+3][j-3] {
-                sum += 1
-            }
-            // west
-            if j-4 >= -1 && "XMAS" == grid[i][j]+grid[i][j-1]+grid[i][j-2]+grid[i][j-3] {
-                sum += 1
-            }
-            // northwest
-            if i-4 >= -1 && j-3 >= 0 && "XMAS" == grid[i][j]+grid[i-1][j-1]+grid[i-2][j-2]+grid[i-3][j-3] {
-                sum += 1
-            }
-            // north
-            if i-4 >= -1 && "XMAS" == grid[i][j]+grid[i-1][j]+grid[i-2][j]+grid[i-3][j] {
-                sum += 1
-            }
-            // northeast
-            if i-4 >= -1 && len(grid[i]) >= j+4 && "XMAS" == grid[i][j]+grid[i-1][j+1]+grid[i-2][j+2]+grid[i-3][j+3] {
-                sum += 1
-            }
+			// east
+			if len(grid[i]) >= j+4 && "XMAS" == grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i][j+3] {
+				sum += 1
+			}
+			// southeast
+			if len(grid) >= i+4 && len(grid[i]) >= j+4 && "XMAS" == grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2]+grid[i+3][j+3] {
+				sum += 1
+			}
+			// south
+			if len(grid) >= i+4 && "XMAS" == grid[i][j]+grid[i+1][j]+grid[i+2][j]+grid[i+3][j] {
+				sum += 1
+			}
+			// southwest
+			if len(grid) >= i+4 && j-4 >= -1 && "XMAS" == grid[i][j]+grid[i+1][j-1]+grid[i+2][j-2]+grid[i+3][j-3] {
+				sum += 1
+			}
+			// west
+			if j-4 >= -1 && "XMAS" == grid[i][j]+grid[i][j-1]+grid[i][j-2]+grid[i][j-3] {
+				sum += 1
+			}
+			// northwest
+			if i-4 >= -1 && j-3 >= 0 && "XMAS" == grid[i][j]+grid[i-1][j-1]+grid[i-2][j-2]+grid[i-3][j-3] {
+				sum += 1
+			}
+			// north
+			if i-4 >= -1 && "XMAS" == grid[i][j]+grid[i-1][j]+grid[i-2][j]+grid[i-3][j] {
+				sum += 1
+			}
+			// northeast
+			if i-4 >= -1 && len(grid[i]) >= j+4 && "XMAS" == grid[i][j]+grid[i-1][j+1]+grid[i-2][j+2]+grid[i-3][j+3] {
+				sum += 1
+			}
 		}
 	}
 	fmt.Println("sum: ", sum)
--- 2024/04/p2/main.go
+++ 2024/04/p2/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"internal/util"
-    "strings"
+	"strings"
 )
 
 func main() {
@@ -12,7 +12,7 @@ func main() {
 	for i := 1; i < len(grid)-1; i++ {
 		for j := 1; j < len(grid[i])-1; j++ {
 			val := grid[i+1][j+1] + grid[i-1][j-1] + grid[i+1][j-1] + grid[i-1][j+1]
-            if grid[i][j] == "A" && strings.Contains("MSMS;MSSM;SMMS;SMSM", val) {
+			if grid[i][j] == "A" && strings.Contains("MSMS;MSSM;SMMS;SMSM", val) {
 				sum += 1
 			}
 		}
